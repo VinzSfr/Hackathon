98a70ccefe1b50add5007f788e54330fcf8435ba
andre <andrebad91@hotmail;fr>
2020-04-04 16:56:09 +0200

    Module fonctionnant pour barrecode

--- app/Views/barcode/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$string_a_coder = "1234516143";
-
-//mettre control=0 pour ne pas utiliser le caractère de controle
-echo "<img src='barecodeController.php?string=".htmlentities($string_a_coder,ENT_QUOTES,"ISO8859-1") . "&amp;control=1'/>";
-?>
